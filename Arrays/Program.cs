b469b6e586ca49881121faab083ee6feb8b38d3e
Rasheed K Mozaffar <rasheedkmozaffar@hotmail.com>
2025-07-16 11:38:27 +0300

    Demonstrate the use of the array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LowLevelArrays;
+
+// Create new double array
+using var array = new LowLevelArray<double>(5);
+
+// Initialize the array values .
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = i + 1;
+}
+
+// Create a copy of the first array.
+using var copy = new LowLevelArray<double>(array);
+
+for (int i = 0; i < copy.Length; i++)
+{
+    Console.WriteLine(copy[i]);
+}
+
+// Create an array with the given values.
+using var initialized = new LowLevelArray<int>([1, 2, 3, 4, 5]);
+
+for (int i = 0; i < initialized.Length; i++)
+{
+    Console.WriteLine(initialized[i]);
+}
