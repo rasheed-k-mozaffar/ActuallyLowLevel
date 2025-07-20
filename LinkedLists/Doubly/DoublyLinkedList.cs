2b7cc266f3be64602c50a34c6a8e94e6c525cf2c
Rasheed K Mozaffar <rasheedkmozaffar@hotmail.com>
2025-07-20 23:47:11 +0300

    Implement adding in doubly linked list

--- LinkedLists/Doubly/DoublyLinkedList.cs
+++ LinkedLists/Doubly/DoublyLinkedList.cs
@@ -1,8 +1,98 @@
 using System;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+using LowLevelLinkedLists.Doubly;
 
 namespace LowLevelLinkedLists;
 
-public unsafe class DoublyLinkedList
+public unsafe class DoublyLinkedList : IDisposable
 {
+    private bool _disposed = false;
 
+    // Pointer to the first node in the list
+    private DoublyNode* _head = null;
+
+    // Pointer to the last node in the list
+    private DoublyNode* _tail = null;
+
+
+    /// <summary>
+    /// Adds a new item to the list at the end
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="data"></param>
+    public void Add(int data)
+    {
+        // Allocate memory for the new node.
+        DoublyNode* newNode = (DoublyNode*)NativeMemory.AllocZeroed((nuint)sizeof(DoublyNode));
+        newNode->Data = data;
+        // Ensure new node's next doesn't point to anything
+        newNode->Previous = null;
+        newNode->Next = null;
+
+        // If the list is empty, head and tail will both point to the same node.
+        if (_head is null)
+        {
+            _head = newNode; // Set head to new node
+            _tail = newNode; // Set tail to new node
+            return;
+        }
+
+        // Set current tail's Next to the new node.
+        _tail->Next = newNode;
+        // Link the new node to the previous node (Ex tail)
+        newNode->Previous = _tail;
+        // Update the tail to point to the new node
+        _tail = newNode;
+    }
+
+    public void Add(int atIndex, int data)
+    {
+
+    }
+
+
+
+
+    public void Display()
+    {
+        Console.Write("Head <-> ");
+        DoublyNode* current = _head;
+        while (current != null)
+        {
+            Console.Write($"[Data: {current->Data} | Addr: {(long)current:X}] <-> ");
+            current = current->Next;
+        }
+        Console.WriteLine("NULL");
+    }
+
+
+    public void DisplayReversed()
+    {
+        Console.WriteLine("Tail <-> ");
+        DoublyNode* current = _tail;
+        while (current is not null)
+        {
+            Console.Write($"[Data: {current->Data} | Addr: {(long)current:X}] <-> ");
+            current = current->Previous;
+        }
+        Console.WriteLine("NULL");
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+            return;
+    }
+
+    ~DoublyLinkedList()
+    {
+        Dispose(false);
+    }
 }
--- LinkedLists/Doubly/DoublyNode.cs
+++ LinkedLists/Doubly/DoublyNode.cs
@@ -2,7 +2,20 @@ using System;
 
 namespace LowLevelLinkedLists.Doubly;
 
-public struct DoublyNode
+public unsafe struct DoublyNode
 {
+    /// <summary>
+    /// The data stored in the node.
+    /// </summary>
+    public int Data;
 
+    /// <summary>
+    /// A pointer to the next node in the list.
+    /// </summary>
+    public DoublyNode* Next;
+
+    /// <summary>
+    /// A pointer to the previous node in the list.
+    /// </summary>
+    public DoublyNode* Previous;
 }
